400194336c1f130e4c20297e499691d38f29ad23
SWH03403 <106224482@student.swin.edu.au>
2025-11-02 13:57:42 +0700

    refactor: allow `data` to be overriden by custom `render` calls

--- src/parts/wraps/top.php
+++ src/parts/wraps/top.php
@@ -3,9 +3,9 @@
 <head class="flex-y">
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title><?= $opts['full_title'] ?? ("Dod - " . ($opts['title'] ?? 'Untitled')) ?></title>
+	<title><?= $data['full_title'] ?? ("Dod - " . ($data['title'] ?? 'Untitled')) ?></title>
 	<link rel="icon" type="image/png" href="/static/images/favicon.png">
-	<link rel="stylesheet" href="/static/css/<?= $opts['style'] ?? 'global' ?>.css">
+	<link rel="stylesheet" href="/static/css/<?= $data['style'] ?? 'global' ?>.css">
 </head>
 <body class="flex-y">
 <?php render('wraps/header') ?>
