4e804867e02c33f8309da9b135cd2dbd51972778
SWH03403 <106224482@student.swin.edu.au>
2025-11-07 16:02:52 +0700

    refactor(init): php binary check & finder

--- app.sh
+++ app.sh
@@ -6,21 +6,21 @@ ADDRESS='127.0.0.1:9524'
 
 _err() { echo "Err: $1!" >&2; exit 1; }
 
-for php in \
-	"$(which php 2>dev/null)" \
-	'/bin/php' \
-	'/usr/bin/php' \
-	'/c/xampp/php/php' \
-	'/c/program files/php/php' \
-	'/c/program files (x86)/php/php'
-do
-	[ -x "$php" ] && break
-done
-
-if [ -z "$php" ]; then
+find_php() {
+	for php in \
+		'/bin/php' \
+		'/usr/bin/php' \
+		'/c/xampp/php/php' \
+		'/c/program files/php/php' \
+		'/c/program files (x86)/php/php'
+	do
+		[ -x "$php" ] && return
+	done
 	echo 'Error: Failed to find `php` executable!'
 	exit 1
-fi
+}
+
+has_php && php='php' || find_php
 
 [ $# -gt 0 ] && exec "$php" "$ENTRY" "$@"
 exec "$php" --server "$ADDRESS" "$ENTRY"
