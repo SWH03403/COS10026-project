53146b28ce34328074efe2a42eb2aae026e992c7
SWH03403 <106224482@student.swin.edu.au>
2025-11-11 08:03:21 +0700

    refactor(wraps): consistent page footer

--- src/parts/wraps/footer.php
+++ src/parts/wraps/footer.php
@@ -1,6 +1,4 @@
 <footer class="flex flex-o">
 	<a href="https://dueordie.atlassian.net/jira/software/projects/WEBTECHPRJ/summary">Project Management</a>
-<?php if (Request::path() == ''): ?>
 	<a href="https://github.com/SWH03303/swh03303.github.io">Github Repo</a>
-<?php endif ?>
 </footer>
