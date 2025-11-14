bdf7ce834c25243bc0650561583a44bad3229175
SWH03403 <106224482@student.swin.edu.au>
2025-11-14 15:31:21 +0700

    feat: application info on profile page

--- src/parts/profile.php
+++ src/parts/profile.php
@@ -1,12 +1,16 @@
-<?php $acc = $data['user']->account(); ?>
+<?php
+$acc = $data['user']->account();
+$apl = $data['applicant'];
+$new = is_null($apl);
+?>
 <h1>Hi, <?= $acc->display ?></h1>
-<article id="account-info" class="box flex-y">
+<article id="account-container" class="box flex-y">
 	<div class="flex">
 		<h2>Account Info</h1>
 		<div class="fill"></div>
-		<a id="account-edit" class="flex-y" href="/user/edit">Edit</a>
+		<a class="edit-btn flex-y" href="/user/edit">Edit</a>
 	</div>
-	<div id="account-grid">
+	<div id="account-info">
 		<span>Email:</span>
 		<span><?= $acc->email ?></span>
 		<span>Created at:</span>
@@ -16,4 +20,22 @@
 		<span>Is manager:</span>
 		<span><?= $acc->is_manager? 'Yes' : 'No' ?></span>
 	</div>
+</article>
+<article id="applicant-container" class="box flex-y">
+	<div class="flex">
+		<h2>Applicant Info</h1>
+		<div class="fill"></div>
+		<a class="edit-btn flex-y" href="<?= $new? '/apply' : '/apply/edit' ?>">
+			<?= $new? 'Apply' : 'Edit' ?>
+		</a>
+	</div>
+	<?php if (is_null($apl)): ?>
+		<span id="no-applicant-message">
+			You currently have not created your application profile. To make one, please click the
+			<span class="important">"Apply"</span> button above!
+		</span>
+	<?php else: ?>
+		<div id="applicant-info">
+		</div>
+	<?php endif; ?>
 </article>
